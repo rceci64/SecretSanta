b82ca616903ea92fa2f436153d031551e505d422
rceci64 <roger.cecilia@students.salle.url.edu>
2020-11-30 21:31:27 +0100

    Animated square

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 console.log("lmao");
+
+window.onload = () => {
+    document.getElementById('boto').onclick = (ev) => {
+        //console.log(ev);
+        console.log('clic');
+        //var lmao = ev.target;
+        console.log(ev.target);
+    }
+};
+
+
+  
+     
+  
+  
+  
