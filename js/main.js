a80cd2b278760d45c96ba720a5eec507cbd51fe6
rceci64 <roger.cecilia@students.salle.url.edu>
2020-12-01 23:49:42 +0100

    Cool animation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
 console.log("lmao");
 
 window.onload = () => {
+    document.querySelector('#dialogbox').style.visibility = "hidden";
     document.getElementById('boto').onclick = (ev) => {
         //console.log(ev);
         console.log('clic');
-        //var lmao = ev.target;
+        
+        
+        
+        if (document.querySelector('#dialogbox').style.visibility == 'hidden'){
+            document.querySelector('#dialogbox').style.visibility = 'visible';
+
+        } else {
+            document.querySelector('#dialogbox').style.visibility = 'hidden';
+        }
+       
+
         console.log(ev.target);
     }
+
+
+
+    var animation = anime({
+        targets: '.square',
+        translateY: -50,
+        direction: 'alternate',
+        easing: 'easeInOutSine',
+        duration: 150,
+        autoplay: false
+    }); 
+    
+    document.querySelector('.square').onclick = animation.restart
+
 };
+
 
 
   
