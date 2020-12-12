68b4d7893955831df9d624bb4d7e371f2f8ef53d
Gerard Parareda Gallifa <parareda8@gmail.com>
2020-12-12 18:59:48 +0100

    Afegit gestor de música amb Howler i les quatre cançons que tenim

---
+++ js/music.js
@@ -0,0 +1,50 @@
+var songs = {
+    
+    mainMusic: new Howl({
+        src: ['../assets/sfx/examn.mpeg'],
+        loop: true,
+        volume: 1.0
+    }),
+    examnMusic: new Howl({
+        src: ['../assets/sfx/examn.mpeg'],
+        loop: true,
+        volume: 1.0,
+    }),
+    winMusic: new Howl({
+        src: ['../assets/sfx/win.mpeg', '../assets/sfx/lose.mpeg'],
+        volume: 1.0,
+    }),
+    loseMusic: new Howl({
+        src: ['../assets/sfx/fail.mpeg'],
+        volume: 1.0,
+    })
+};
+
+var changeVolume = (vol) => {
+    Howler.volume(vol);
+}
+
+var changeSong = (nextSong) => {
+    currentSong.fade(Howler.volume(), 0.0, 2000).on(
+        "fade", () => {
+                currentSong.stop();
+                nextSong.play();
+                nextSong.fade(0.0, Howler.volume(), 3000).on(
+                    "fade", () => {
+                        currentSong = nextSong;
+                    }
+                )
+            }
+        );
+}
+
+var mainMusic = songs['mainMusic'];
+var examnMusic = songs['examnMusic'];
+var winMusic = songs['winMusic'];
+var loseMusic = songs['loseMusic'];
+
+var lastSong = null;
+var currentSong = loseMusic;
+
+mainMusic.play();
+
